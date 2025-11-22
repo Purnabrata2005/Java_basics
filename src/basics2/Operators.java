8fe6e6f79eed9ee260372075c37251c8a1fc4c76
Purnabrata2005 <deypurnabrata2005@gmail.com>
2025-11-22 20:18:16 +0530

    basics of Operators and use of Scanner function

---
+++ src/basics2/Operators.java
@@ -0,0 +1,41 @@
+package basics2;
+
+public class Operators {
+
+   public static void main(String[] args) {
+        int a = 15 ;
+        int b = 14 ;
+        int c = a+b ;
+        int d = a-b ;
+        int e = a*b ;
+        double f = a/(double)b ;  // for float value
+
+//       System.out.println(c);
+//       System.out.println(f);
+
+       /* We have multiple  operators lika{
+       '=','+=','-=','*=','/=','%='
+       */
+
+       /* We have also Relational Operators like {
+       '==','!=','>','<','>=','<='
+        }
+       */
+
+       /*We have Logical Operators like {
+       '&&','||','!'
+       }
+        */
+
+       /*
+       We have also BiteWise Operators
+        */
+
+       int firstNum = 12 ;
+       int secondNum = 16;
+
+//       System.out.println(firstNum == secondNum); // it gives us false
+
+
+    }
+}
---
+++ src/basics2/UserInput.java
@@ -0,0 +1,13 @@
+package basics2;
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Yor age:  ");
+        int age = sc.nextInt();
+        System.out.println("Your age is "+age);
+        sc.close(); // cline the sc method
+
+    }
+}
